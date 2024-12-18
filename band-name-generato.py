584b50a29b1d275ad3502750d39d49d81164e24d
Antara Chanda <antara00418@gmail.com>
2024-12-18 18:12:01 +0530

    Initial commit

---
+++ band-name-generato.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator!")
+
+city_name = input("Which city did you grow up in? ")
+
+pet_name = input("What is the name of your pet? ")
+
+band_name = city_name + " " + pet_name
+
+print(f"Your band name could be: {band_name}")
